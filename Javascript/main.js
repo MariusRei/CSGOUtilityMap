819889e135fbaf59d5d5ae9078f795b18864fc1c
Marius Reinhold <marius.reinhold@raumtaenzer.com>
2020-06-18 19:52:56 +0200

    Initial Commit

---
+++ Javascript/main.js
@@ -0,0 +1,24 @@
+function myFunction(elem) {
+  var x = document.getElementById("js-description");
+  var description = elem.getAttribute('data-description');
+  x.innerHTML = description;
+
+  var button = document.getElementsByClassName('js-button');
+
+  for (var i = 0; i < button.length; i++) {
+    button[i].classList.remove('active-button');
+  }
+
+  elem.classList.add('active-button');
+}
+
+
+function toggleUtil(util) {
+  $(".utilities").hide();
+  $("#" + util).show();
+}
+
+
+function myFunction() {
+  alert("You clicked the coffee cup!");
+}
